17a01ed7b8da65b50eaaa3bbac4fea2df7a633da
zmy100 <zhangmengying0703@hotmail.com>
2023-12-07 15:00:05 +0800

    vue基础8练习

---
+++ day065作业/Vue基础8/demo/src/ToDo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input v-model="inputValue" type="text" />
+    <button @click="handleClick">提交</button>
+  </div>
+  <ul>
+    <list-item v-for="(item, index) of list" :key="index" :item="item" />
+  </ul>
+</template>
+
+<script>
+import { reactive, ref } from "vue";
+// 引入组件
+import ListItem from "./components/ListItem.vue";
+export default {
+  // 注册组件
+  components: {
+    "list-item": ListItem,
+  },
+  name: "ToDo",
+  setup() {
+    const inputValue = ref("");
+    const list = reactive([]);
+    const handleClick = () => {
+      list.push(inputValue.value);
+      inputValue.value = "";
+    };
+    return {
+      inputValue,
+      list,
+      handleClick,
+    };
+  },
+};
+</script>
+
+<style>
+button {
+  margin-left: 20px;
+}
+li {
+  color: red;
+}
+</style>
---
+++ day065作业/Vue基础8/demo/src/components/ListItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>{{ item }}</li>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  props: {
+    item: String,
+  },
+};
+</script>
+
+<style></style>
